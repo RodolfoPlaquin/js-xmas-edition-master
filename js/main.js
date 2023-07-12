e580eab302af80c20042b06687a53fc358e069c4
RodolfoPlaquin <135379012+RodolfoPlaquin@users.noreply.github.com>
2023-07-12 18:48:39 -0300

    Cambia descripción en los textos de los errores

--- js/main.js
+++ js/main.js
@@ -1,45 +1,69 @@
 function validarNombre(nombre) {
   if (nombre.length === 0) {
-    return "Este campo debe tener al menos 1 caracter";
+    return "El nombre debe tener al menos 1 caracter";
   }
 
   if (nombre.length >= 50) {
-    return "Este campo debe tener menos de 50 caracteres";
+    return "El nombre debe tener menos de 50 caracteres";
   }
 
-  if(!/^[A-Za-z]+$/u.test(nombre)){
-    return "Este campo solo admite letras";
+  if (!/^[A-Za-z]+$/.test(nombre)) {
+    return "El nombre solo admite letras";
   }
   return "";
 }
 
 function validarCiudad(ciudad) {
   if (ciudad.length === 0) {
-    return "El campo no debe estar vacío";
+    return "El campo de ciudad no debe estar vacío";
   }
   return "";
 }
 
 function validarDescripcionRegalo(descripcionRegalo) {
   if (descripcionRegalo.length === 0) {
-    return "El campo no debe estar vacío";
+    return "El campo de descripción de regalo no debe estar vacío";
   }
 
   if (descripcionRegalo.length >= 100) {
-    return "Este campo debe tener menos de 100 caracteres";
+    return "El campo de descripción de regalo debe tener menos de 100 caracteres";
   }
 
-  if(!/^[a-z0-9]+$/i.test(descripcionRegalo)){
-    return "Este campo solo admite letras y números";
+  if (!/^[a-z0-9]+$/i.test(descripcionRegalo)) {
+    return "El campo de descripción de regalo solo admite letras y números";
   }
   return "";
 }
 
-function validarFormulario(e){
-  const $form = document.querySelector('#carta-a-santa');
+function manejarErrores(errores) {
+  const keys = Object.keys(errores);
+  const $errores = document.querySelector("#errores");
+  $errores.innerHTML = "";
+  let cantidadErrores = 0;
+
+  keys.forEach(function (key) {
+    const error = errores[key];
+
+    if (error) {
+      cantidadErrores++;
+      $form[key].className = "error";
+
+      const $error = document.createElement("li");
+      $error.innerText = error;
+      $errores.appendChild($error);
+    } else {
+      $form[key].className = "";
+    }
+  });
+}
+
+const $form = document.querySelector("#carta-a-santa");
+$form.onclick = function (e) {
+  e.preventDefault();
+  const $form = document.querySelector("#carta-a-santa");
   const nombre = $form.nombre.value;
   const ciudad = $form.ciudad.value;
-  const descripcionRegalo = $form['descripcion-regalo'].value;
+  const descripcionRegalo = $form["descripcion-regalo"].value;
 
   const errorNombre = validarNombre(nombre);
   const errorCiudad = validarCiudad(ciudad);
@@ -48,35 +72,8 @@ function validarFormulario(e){
   const errores = {
     nombre: errorNombre,
     ciudad: errorCiudad,
-    "descripcion-regalo": errorDescripcionRegalo
-  }
+    "descripcion-regalo": errorDescripcionRegalo,
+  };
 
   manejarErrores(errores);
-
-  e.preventDefault();
-}
-
-function manejarErrores(errores){
-  const keys = Object.keys(errores);
-  const $errores = document.querySelector('#errores');
-  const $error = document.createElement('li');
-  $error.innerHTML = "";
-  let cantidadErrores = 0;
-
-  keys.forEach(function(key){
-    const error = errores[key];
-
-    if(error){
-      cantidadErrores++;
-      $form[key].className = "error";
-
-      $error.innerText = error;
-      $errores.appendChild($error); 
-    } else {
-      $form[key].className = "";
-    }
-  })
-}
-
-const $form = document.querySelector('#carta-a-santa');
-$form.onsubmit = validarFormulario;
+};
--- js/pruebas.js
+++ js/pruebas.js
@@ -1,13 +1,13 @@
 function probarValidarNombre() {
   console.assert(
-    validarNombre("") === "Este campo debe tener al menos 1 caracter",
+    validarNombre("") === "El nombre debe tener al menos 1 caracter",
     "Validar nombre no validó que el nombre no sea vacío"
   );
 
   console.assert(
     validarNombre(
       "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
-    ) === "Este campo debe tener menos de 50 caracteres",
+    ) === "El nombre debe tener menos de 50 caracteres",
     "Validar nombre no validó que el nombre sea menor a 50 caracteres"
   );
 
@@ -24,37 +24,38 @@ function probarValidarNombre() {
 
 function probarValidarCiudad() {
   console.assert(
-    validarCiudad("Buenos Aires") === "",
-    "ValidarCuidad no funcionó con un nombre válido "
+    validarCiudad("") === "El campo ciudad no debe estar vacío",
+    "ValidarCiudad no validó que el campo ciudad no sea vacío"
   );
 
   console.assert(
-    validarCiudad("") === "Este campo no debe estar vacío",
-    "ValidarCiudad no validó que el campo ciudad no sea vacío"
+    validarCiudad("Buenos Aires") === "",
+    "ValidarCiudad no funcionó con un nombre de ciudad válido "
   );
 }
 
 function probarValidarDescripcionRegalo() {
   console.assert(
-    validarDescripcionRegalo("Descripción de regalo") === "",
-    "ValidarDescripcionRegalo no funcionó con un nombre válido "
-  );
-
-  console.assert(
     validarDescripcionRegalo(
-      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    ) === "ValidarDescripcionRegalo no validó que el nombre sea menor a 100 caracteres",
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    ) === "La descripcion del regalo debe tener menos de 100 caracteres",
     "ValidarDescripcionRegalo no funcionó con un nombre válido "
   );
 
   console.assert(
-    validarDescripcionRegalo("") === "Este campo no debe estar vacío",
-    "ValidarDescripcionRegalo no validó que el campo descripcionRegalo no sea vacío"
+    validarDescripcionRegalo("") ===
+      "El campo de la descripción de regalo no debe estar vacío",
+    "ValidarDescripcionRegalo no validó que el campo descripcion de regalo no sea vacío"
   );
 
   console.assert(
-    validarDescripcionRegalo(",.,.,") === "Este campo solo admite letras y números",
+    validarDescripcionRegalo(",.,.,") ===
+      "La descripción de regalo solo admite letras y números",
     "ValidarDescripcionRegalo no validó que el campo solo tenga letras y números"
+  );
+  console.assert(
+    validarDescripcionRegalo("Descripcion de regalo") === "",
+    "ValidarDescripcionRegalo no funcionó con un nombre válido "
   );
 }
 
